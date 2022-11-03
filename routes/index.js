44fc59d7594e5a76231d5e0b0886b3046f9869cb
Javier Sanchez <jsanchezsv95@gmail.com>
2022-11-03 17:47:43 -0600

    app

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express'); //
 var router = express.Router();
-require('dotenv').config
+require('dotenv').config();
 
 var pg = require('pg');
 var data;
@@ -16,21 +16,25 @@ const connectionData = {
 
 const client = new pg.Client(connectionData);
 
-client.connect()
-client.query('SELECT * FROM contacts')
-  .then(response => {
-    console.log(response.rows)
-    data = response.rows
-    client.end
-  })
-  .catch(err => {
-    client.end()
-  })
+client.connect(function(err){
+  if(err) throw err;
+  console.log("connected");
+  }
+);
+//client.query('SELECT * FROM contacts')
+//  .then(response => {
+//    console.log(response.rows)
+//    data = response.rows
+//    client.end
+//  })
+//  .catch(err => {
+//    client.end()
+//  })
 
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Contacts', items: data.map(item =>{
+  res.render('index', { title: 'Contacts'/*, items: data.map(item =>{
     return ["Name: ",item.username,"Phone: ", item.phone].join(" ");
-  })});
+  })*/});
 });
 
 module.exports = router;
