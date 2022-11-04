f84668bfe5f3b5c6acd1047dde37aa09792b33ef
Javier Sanchez <jsanchezsv95@gmail.com>
2022-11-04 08:44:08 -0600

    app

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
+
 var express = require('express'); //
 var router = express.Router();
-require('dotenv').config();
 
 var pg = require('pg');
 var data;
+
+console.log("Hostname: %s, username: %s, password: %s", process.env.RDS_HOSTNAME, process.env.RDS_USERNAME, process.env.RDS_PASSWORD)
 
 const connectionData = {
   host: process.env.RDS_HOSTNAME,
@@ -11,25 +14,62 @@ const connectionData = {
   password: process.env.RDS_PASSWORD,
   port: process.env.RDS_PORT,
   database: process.env.RDS_DATABASE
-
 };
 
 const client = new pg.Client(connectionData);
+
+client.connect();     // gets connection
+
+const execute = (query) => {
+  try {
+    client.query(query);  // sends queries
+    return true;
+  } catch (error) {
+    console.error(error.stack);
+    return false;
+  } finally {
+  }
+};
+
+const create_table = `
+    CREATE TABLE IF NOT EXISTS contacts (
+	    "id" SERIAL,
+	    "name" VARCHAR(100) NOT NULL,
+	    "phone" VARCHAR(15) NOT NULL,
+	    PRIMARY KEY ("id")
+    );`;
+
+client.query(create_table).then(result => {
+  if (result) {
+    console.log('Table created');
+  }
+});
+
+const insert_record = `INSERT INTO contacts(name, phone) values ('Usuario 1', '77777777')`;
+
+client.query(insert_record).then(result => {
+  if (result) {
+    console.log('Record added');
+  }
+});
 
 client.query('SELECT * FROM contacts')
   .then(response => {
     console.log(response.rows)
     data = response.rows
-    client.end
   })
   .catch(err => {
-    client.end()
-  })
+    console.error(error.stack);
+  });
+
+// client.end();         // closes connection
 
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Contacts', items: data.map(item =>{
-    return ["Name: ",item.username,"Phone: ", item.phone].join(" ");
-  })});
+  res.render('index', {
+    title: 'Contacts', items: data.map(item => {
+      return ["Name: ", item.username, "Phone: ", item.phone].join(" ");
+    })
+  });
 });
 
 module.exports = router;
