41dc9ac4375c07f18089301e860f60e9bbf4bbbf
Javier Sanchez <jsanchezsv95@gmail.com>
2022-11-03 20:30:16 -0600

    app

--- routes/index.js
+++ routes/index.js
@@ -16,25 +16,20 @@ const connectionData = {
 
 const client = new pg.Client(connectionData);
 
-client.connect(function(err){
-  if(err) throw err;
-  console.log("connected");
-  }
-);
-//client.query('SELECT * FROM contacts')
-//  .then(response => {
-//    console.log(response.rows)
-//    data = response.rows
-//    client.end
-//  })
-//  .catch(err => {
-//    client.end()
-//  })
+client.query('SELECT * FROM contacts')
+  .then(response => {
+    console.log(response.rows)
+    data = response.rows
+    client.end
+  })
+  .catch(err => {
+    client.end()
+  })
 
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Contacts'/*, items: data.map(item =>{
+  res.render('index', { title: 'Contacts'*, items: data.map(item =>{
     return ["Name: ",item.username,"Phone: ", item.phone].join(" ");
-  })*/});
+  })});
 });
 
 module.exports = router;
